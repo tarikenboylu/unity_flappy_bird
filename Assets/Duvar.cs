ea7e47f3a7d55decd38b1e1e35ffb0b0a0e8bc10
tarikenboylu <tarikenboylu@gmail.com>
2020-01-02 17:52:12 +0300

    initial commit

---
+++ Assets/Duvar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duvar : MonoBehaviour
+{
+    public GameObject Wall;
+    float createTime = 5;
+    float Songecensure;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Songecensure = createTime - Time.time;
+        if (Songecensure < 0)
+        {
+            createTime += Random.Range(2, 5);
+            Instantiate(Wall, transform.position + new Vector3(0, Random.Range(-8, 8), 0), Quaternion.Euler(0, 0, 0));
+        }
+    }
+}
---
+++ Assets/MoveCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.A))
+            //rb.velocity = new Vector2(-2, rb.velocity.y);
+        //if(Input.GetKey(KeyCode.D))
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        if(Input.touchCount > 0)
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+
+    }
+}
